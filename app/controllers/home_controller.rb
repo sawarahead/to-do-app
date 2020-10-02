80de02e0b0172d819aef2de2fc2745b48b28addb
小山智暉 <koyamatomoki@koyamasatoshiakiranoMacBook-puro.local>
2020-10-02 16:12:44 +0900

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
 
 require "date"
-require "uri"
-require "net/http"
+
 
   def top
 
@@ -18,42 +17,6 @@ require "net/http"
     @repeat=["日","月","火","水","木","金","土","毎","単"]
     @total_task_time=Task.where("date=?",@today).sum(:time)
     @place=["未定","自宅"]
-  end
-
-  def line_login
-    uri=URI.parse("https://api.line.me/oauth2/v2.1/token")
-    request=Net::HTTP::Post.new(uri)
-    request.content_type="application/x-www-form-urlencoded"
-    request.set_form_data(
-      "grant_type" => "authorization_code",
-      "code" => params[:code],
-      "redirect_uri" => "https://infinite-fjord-36648.herokuapp.com/",
-      "client_id" => "1654987275",
-      "client_secret" => "606b3608ff10c18bc0c1d92a575d355c"
-    )
-
-    req_options = {
-      use_ssl: uri.scheme == "https"
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    JWT.decode(JSON.parse(response.body)["id_token"],"#{client_secret}")
-
-    @user=User.find_by(name:response.name)
-
-    if @user
-      session[:user_id] = @user.id
-      redirect_to("/home/index")
-    else
-      @user_new=User.new(name:response.name)
-      @user_new.save
-      @user_session=User.find_by(name:response.name)
-      session[:user_id] = @user_session.id
-      redirect_to("/home/index")
-    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   post 'events/:id/update' => "events#update"
 
   post 'callback' => "linebot#callback"
-  post 'auth' => "home#line_login"
+  post 'auth' => "line_login#line_login"
 
   get 'home/index'
   get '/' => 'home#top'

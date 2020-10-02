cb6e8b5ad5f183c28547c8fbeec7e6b2b4f5dc45
小山智暉 <koyamatomoki@koyamasatoshiakiranoMacBook-puro.local>
2020-10-03 04:19:08 +0900

    Update commit

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   post 'events/:id/update' => "events#update"
 
   post 'callback' => "linebot#callback"
-  get 'auth' => "line_login#auth_top"
+  post 'auth' => "line_login#auth_top"
   post 'line_login' => "line_login#line_login"
 
   get 'home/index'

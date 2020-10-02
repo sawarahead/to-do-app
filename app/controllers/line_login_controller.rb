8f0ea5fd52b756432b4516366d6d148918795abb
小山智暉 <koyamatomoki@koyamasatoshiakiranoMacBook-puro.local>
2020-10-03 03:00:32 +0900

    Update commit

--- app/controllers/line_login_controller.rb
+++ app/controllers/line_login_controller.rb
@@ -28,7 +28,7 @@ class LineLoginController < ApplicationController
 
     uri=URI.parse("https://api.line.me/v2/profile")
     request=Net::HTTP::Get.new(uri)
-    request.authorization="Bearer {access token}"
+    request["Authorization"]="Bearer {access token}"
     req_options = {
       use_ssl: uri.scheme == "https"
     }

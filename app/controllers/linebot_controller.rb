6ff9c768879928eb4425e07a92e3c45037490ec9
小山智暉 <koyamatomoki@koyamasatoshiakiranoMacBook-puro.local>
2020-11-19 01:44:46 +0900

    LINE messaging bot編集

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -43,7 +43,7 @@ class LinebotController < ApplicationController
         if user
           if user.authenticate(event['source']['userId'])
 
-              response="本日のto-do:\n#{tasks.where(user_id:user.id).pluck(:content).each{|task| puts task.join}}\n本日のevent:\n#{plans.where(user_id:user.id).pluck(:content)}}"
+              response="本日のto-do:\n#{tasks.where(user_id:user.id).pluck(:content).each{|task| puts task.join("、")}}\n本日のevent:\n#{plans.where(user_id:user.id).pluck(:content)}}"
 
           else
             response="該当するユーザー名は存在しますが、データにアクセスする権限がありません。"
